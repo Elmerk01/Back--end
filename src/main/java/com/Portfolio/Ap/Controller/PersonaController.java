eb2ed9fae08366e659b96788c794eea09adc3f64
Alejo Diaz <101262131+Elmerk01@users.noreply.github.com>
2023-01-04 08:58:52 -0300

    actualizacion
    system.properties

--- src/main/java/com/Portfolio/Ap/Controller/PersonaController.java
+++ src/main/java/com/Portfolio/Ap/Controller/PersonaController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontap-7836e.web.app")
 public class PersonaController {
     @Autowired
     ImpPersonaService personaService;
--- src/main/java/com/Portfolio/Ap/Security/Controller/AuthController.java
+++ src/main/java/com/Portfolio/Ap/Security/Controller/AuthController.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin (origins = "https://frontap-7836e.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
