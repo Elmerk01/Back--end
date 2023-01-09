aa4b3937a299fda133c87984120439d7c39ae935
Alejo Diaz <101262131+Elmerk01@users.noreply.github.com>
2023-01-09 11:38:36 -0300

    actu. back/front

--- src/main/java/com/Portfolio/Ap/Security/Controller/AuthController.java
+++ src/main/java/com/Portfolio/Ap/Security/Controller/AuthController.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin (origins = "http://localhost:4200")
+@CrossOrigin (origins = "https://frontap-7836e.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
